cf8a79bad7808f39cb99c135a85ce6a8dd6ce99d
Sergio Obolevich <sergioobolevich@gmail.com>
2021-05-06 12:39:06 -0300

    Add Property title and address validation on model.

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,7 @@
 class Property < ApplicationRecord
   has_many :favourites, dependent: :destroy
   has_many :likers, through: :favourites, source: "user"
+
+  validates_uniqueness_of :title, :address
+  validates_presence_of :title, :desc, :price, :bedrooms, :baths, :sqrmeters, :address, :img_url
 end
